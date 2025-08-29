56b033d656021e4330884565df8ddfeb09ca8989
Alisher Temirgali <keykibatyr@gmail.com>
2025-08-30 02:09:51 +0500

    user does not see their own messages twice. When typing commands, they are not shown to others

--- main.go
+++ main.go
@@ -16,15 +16,20 @@ type Client struct {
 type Hub struct {
 	registered   chan *Client
 	deregistered chan *Client
-	broadcast    chan string
+	broadcast    chan Message
 	clients      map[*Client]bool
+}
+
+type Message struct {
+	sender  *Client
+	content string
 }
 
 func newHub() *Hub {
 	return &Hub{
 		registered:   make(chan *Client),
 		deregistered: make(chan *Client),
-		broadcast:    make(chan string),
+		broadcast:    make(chan Message),
 		clients:      make(map[*Client]bool),
 	}
 }
@@ -34,21 +39,24 @@ func (h *Hub) run() {
 		select {
 		case c := <-h.registered:
 			h.clients[c] = true
-			h.sendToAll(fmt.Sprintf("* %s has joined the chat", c.name))
+			h.sendToAll(fmt.Sprintf("* %s has joined the chat", c.name), c)
 		case c := <-h.deregistered:
 			_, ok := h.clients[c]
 			if ok {
 				h.clients[c] = false
-				h.sendToAll(fmt.Sprintf("* %s has left the chat", c.name))
+				h.sendToAll(fmt.Sprintf("* %s has left the chat", c.name), c)
 			}
 		case msg := <-h.broadcast:
-			h.sendToAll(msg)
+			h.sendToAll(msg.content, msg.sender)
 		}
 	}
 }
 
-func (h *Hub) sendToAll(msg string) {
+func (h *Hub) sendToAll(msg string, sender *Client) {
 	for c := range h.clients {
+		if c == sender {
+			continue
+		}
 		select {
 		case c.out <- msg:
 		default:
@@ -95,27 +103,27 @@ func handleCon(h *Hub, conn net.Conn) {
 	sc := bufio.NewScanner(conn)
 	for sc.Scan() {
 		line := sc.Text()
+
 		if len(line) > 6 && line[:5] == "/nick" {
 			old := client.name
 			client.name = line[6:]
 			client.out <- fmt.Sprintf("your nick is now %s", client.name)
-			h.broadcast <- fmt.Sprintf("* %s has changed their nick to --> %s <--", old, client.name)
+			h.broadcast <- Message{content: fmt.Sprintf("* %s has changed their nick to --> %s <--", old, client.name), sender: client}
 
 		}
 
-		if line == "/quit" {
+		switch {
+		case line == "/quit":
 			h.deregistered <- client
 			conn.Close()
 			return
-		}
-
-		if line == "/users" {
+		case line == "/users":
 			for user := range h.clients {
 				client.out <- fmt.Sprintf("the user: %s", user.name)
 			}
+		default:
+			h.broadcast <- Message{content: fmt.Sprintf("[the user %s]: %s", client.name, line), sender: client}
 		}
-
-		h.broadcast <- fmt.Sprintf("[the user %s]: %s", client.name, line)
 	}
 
 	err := sc.Err()
